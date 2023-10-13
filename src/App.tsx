d3e69f749d99351fbfb0d49ce0dbdbe9efd1ccb9
Benni-beep <krikelkr4kel@gmail.com>
2023-10-13 09:08:18 +0200

    feat: add component {NewArticles}

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import { HeadlineArticle } from "./components/HeadlineArticle";
+import { NewArticles } from "./components/NewArticles";
 import { PageHeader } from "./components/PageHeader";
 import { RankedArticleList } from "./components/RankedArticleList";
 
@@ -20,35 +21,8 @@ function App() {
   return (
     <main className="flex-column gap-2-rem">
       <PageHeader toggleOverlay={toggleOverlay} />
-
       <HeadlineArticle />
-
-        <div className="new-articles gap-2-rem">
-          <h2>New</h2>
-          <section>
-            <div>
-              <h3>Hydrogen VS Electric Cars</h3>
-              <p>Will hydrogen-fueled cars ever catch up to EVs?</p>
-            </div>
-            <hr />
-            <div>
-              <h3>The Downsides of AI Artistry</h3>
-              <p>
-                What are the possible adverse effects of on-demand AI image
-                generation?
-              </p>
-            </div>
-            <hr />
-            <div>
-              <h3>Is VC Funding Drying Up?</h3>
-              <p>
-                Private funding by VC firms is down 50% YOY. We take a look at
-                what that means.
-              </p>
-            </div>
-          </section>
-        </div>
-
+      <NewArticles />
       <RankedArticleList />
     </main>
   );
---
+++ src/components/NewArticles.tsx
@@ -0,0 +1,29 @@
+export const NewArticles = () => {
+  return (
+    <div className="new-articles gap-2-rem">
+      <h2>New</h2>
+      <section>
+        <div>
+          <h3>Hydrogen VS Electric Cars</h3>
+          <p>Will hydrogen-fueled cars ever catch up to EVs?</p>
+        </div>
+        <hr />
+        <div>
+          <h3>The Downsides of AI Artistry</h3>
+          <p>
+            What are the possible adverse effects of on-demand AI image
+            generation?
+          </p>
+        </div>
+        <hr />
+        <div>
+          <h3>Is VC Funding Drying Up?</h3>
+          <p>
+            Private funding by VC firms is down 50% YOY. We take a look at what
+            that means.
+          </p>
+        </div>
+      </section>
+    </div>
+  );
+};
