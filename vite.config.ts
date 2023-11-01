510ed95b39e29e669999b0867da937220228522a
Benni-beep <krikelkr4kel@gmail.com>
2023-11-01 19:10:48 +0100

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/frontent_mentor_-_news_homepage/",
+  base: "https://bschlademann.github.io/frontend_mentor_-_news_homepage/",
 });
