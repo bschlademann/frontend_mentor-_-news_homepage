7e765c23e6f3541a9ed0be57d249b94e82c55ceb
Benni-beep <krikelkr4kel@gmail.com>
2023-11-07 09:09:13 +0100

    fix: hero-image replacement

--- src/components/HeadlineArticle.tsx
+++ src/components/HeadlineArticle.tsx
@@ -2,13 +2,14 @@ import "./HeadlineArticle.css";
 export const HeadlineArticle = () => {
   return (
     <div className="headline-article">
-      <img
-        className="hero-image"
-        srcSet="./images/image-web-3-mobile.jpg 1150w, ./images/image-web-3-desktop.jpg 1980w"
-        sizes="(min-width: 1150w) 100%, 100%"
-        src="./images/logo.svg"
-        alt=""
-      />
+  <img
+  className="hero-image"
+  srcSet="./images/image-web-3-mobile.jpg 686w, ./images/image-web-3-desktop.jpg 1460w"
+  sizes="(max-width: 1149px) 686px, 1460px"
+  src="./images/logo.svg"
+  alt=""
+/>
+
       <h1>The Bright Future of Web 3.0?</h1>
       <div>
         <p>
